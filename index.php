e9d510c7ecaaf1e457b180de8bf38769a0506437
nicolasmahlangu75 <nicolasmahlangu75@gmail.com>
2020-03-11 16:10:43 +0200

    master

--- index.php
+++ index.php
@@ -2,79 +2,205 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name = "viewport" content = "width=device-width, initail-scale=1">
     <title>home</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel = "stylesheet" href = "index.css">
+
   </head>
   <body>
 
-<header>
+    <header>
 
-  <nav class="navbar">
+      <nav class = "navbar navbar-default">
 
-      <div class="wrapper">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span class="sr-only">Toggle navi</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
+        <div class = "container">
 
-              <a class="brand" href="#">Nick</a>
+          <div class="container">
 
+              <div class="navbar-header">
+
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+
+                </button>
+
+              </div>
+
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+              <ul class="nav navbar-nav navbar-right">
+
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+
+              </ul>
+
+            </div>
           </div>
+
+          </nav>
+
+        </header>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Services</a></li>
-              <li><a href="#">About</a></li>
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
 
-            </ul>
+              <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="2"></li>
 
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class= "carousel-inner" role="listbox">
+              <div class= "item active">
+                <div class= "banner" style = "background-image: url(images/image_1.jpg);"></div>
+                <div class= "carousel-caption">
+
+                  <h2>Quality Filing System</h2>
+                  <h3>Best File System On Earth</h3>
+                  <p>Filing At Your Service</p>
+
+                </div>
+              </div>
+
+              <div class="item">
+                <div class= "banner" style = "background-image: url(images/image_2.jpg);"></div>
+                <div class="carousel-caption">
+
+                  <h2>Quality Filing System</h2>
+                  <h3>Best File System On Earth</h3>
+                  <p>Filing At Your Service</p>
+
+                </div>
+              </div>
+
+              <div class="item">
+                <div class= "banner" style = "background-image: url(images/image_3.jpg);"></div>
+                <div class="carousel-caption">
+
+                  <h2>Quality Filing System</h2>
+                  <h3>Best File System On Earth</h3>
+                  <p>Filing At Your Service</p>
+
+            </div>
           </div>
+
+              <div class="item">
+                <div class= "banner" style = "background-image: url(images/image_3.jpg);"></div>
+                <div class="carousel-caption">
+
+                  <h2>Quality Filing System</h2>
+                  <h3>Best File System On Earth</h3>
+                  <p>Filing At Your Service</p>
+
+                </div>
+              </div>
+
+              <div class="item">
+                <div class= "banner" style = "background-image: url(images/image_4.jpg);"></div>
+                <div class="carousel-caption">
+
+                  <h2>Quality Filing System</h2>
+                  <h3>Best File System On Earth</h3>
+                  <p>Filing At Your Service</p>
+
+                </div>
+              </div>
+
+              <div class="item">
+                <div class= "banner" style = "background-image: url(images/image_5.jpg);"></div>
+                <div class="carousel-caption">
+
+                  <h2>Quality Filing System</h2>
+                  <h3>Best File System On Earth</h3>
+                  <p>Filing At Your Service</p>
+
+                </div>
+              </div>
+
+              <div class="item">
+                <div class= "banner" style = "background-image: url(images/image_6.jpg);"></div>
+                <div class="carousel-caption">
+
+                  <h2>Quality Filing System</h2>
+                  <h3>Best File System On Earth</h3>
+                  <p>Filing At Your Service</p>
+
+                </div>
+              </div>
+
+              <div class="item">
+                <div class= "banner" style = "background-image: url(images/image_7.jpg);"></div>
+                <div class="carousel-caption">
+
+                  <h2>Quality Filing System</h2>
+                  <h3>Best File System On Earth</h3>
+                  <p>Filing At Your Service</p>
+
+                </div>
+              </div>
+
+              <div class="item">
+                <div class= "banner" style = "background-image: url(images/image_8.jpg);"></div>
+                <div class="carousel-caption">
+
+                  <h2>Quality Filing System</h2>
+                  <h3>Best File System On Earth</h3>
+                  <p>Filing At Your Service</p>
+
+                </div>
+              </div>
+
+              <div class="item">
+                <div class= "banner" style = "background-image: url(images/image_9.jpg);"></div>
+                <div class="carousel-caption">
+
+                  <h2>Quality Filing System</h2>
+                  <h3>Best File System On Earth</h3>
+                  <p>Filing At Your Service</p>
+
+                </div>
+              </div>
+
+              <div class="item">
+                <div class= "banner" style = "background-image: url(images/image_10.jpg);"></div>
+                <div class="carousel-caption">
+
+                  <h2>Quality Filing System</h2>
+                  <h3>Best File System On Earth</h3>
+                  <p>Filing At Your Service</p>
+
+                </div>
+              </div>
+
         </div>
-      </nav>
 
-      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-<!-- Indicators -->
-<ol class="carousel-indicators">
-  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-</ol>
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+            </div>
 
-<!-- Wrapper for slides -->
-<div class="carousel-inner" role="listbox">
-  <div class="item active">
-    <img src="..." alt="...">
-    <div class="carousel-caption">
-      ...
-    </div>
-  </div>
-  <div class="item">
-    <img src="..." alt="...">
-    <div class="carousel-caption">
-      ...
-    </div>
-  </div>
-  ...
-</div>
-
-<!-- Controls -->
-<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-  <span class="sr-only">Previous</span>
-</a>
-<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-  <span class="sr-only">Next</span>
-</a>
-</div>
-
-      </header>
+            <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
         <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
