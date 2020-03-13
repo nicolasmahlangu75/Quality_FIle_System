c49111e2b729fad5a256d7195c2d438599e63f31
nicolasmahlangu75 <nicolasmahlangu75@gmail.com>
2020-03-13 12:05:20 +0200

    master

--- index.php
+++ index.php
@@ -17,14 +17,13 @@
 
           <a class = "navbar-brand" href = "#">Quality File System</a>
 
-
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
 
-        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+        <ul class="navbar-nav ml-auto">
 
           <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
