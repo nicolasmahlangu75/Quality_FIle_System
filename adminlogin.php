dd8eab5e8e6801450d51c92105b9031314ede3df
nicolasmahlangu75 <nicolasmahlangu75@gmail.com>
2020-04-01 01:00:46 +0300

    master

---
+++ adminlogin.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="registration.php">Register</a></li>
+  <li><a href="login.php">Login</a></li>
+  <li><a href="adminlogin.php">Administrator Login</a></li>
+</ul>
+<head>
+<title>Login</title>
+<link rel="stylesheet" type="text/css" href="css/styleslogin.css">
+
+</head>
+
+<div class="bod">
+<body>
+<div class="container">
+
+
+<div id="frm" class="container">
+	<form action="adminlogin_process.php" method="POST">
+	<h1>Login</h1><br>
+	<br>
+	<p>
+	<label> Email </label>
+
+	<input type="text" id="email" name="email_address" />
+	<hr>
+	</p>
+
+<br>
+	<p>
+	<label> Password</label>
+
+	<input type="password" id="password" name="password" />
+	<hr>
+	</p>
+	<p>
+	<input type="submit" id="btn" class="login-btn" value="LOGIN" />
+	</p>
+	<p>
+	Create new Account <a href="registration.php">Register</a>
+				</p>
+	</form>
+</div>
+</body>
+</div>
+<div>
+<div>
+<footer align="center" class="footer">Powered by ICEP  &copy  </footer>
+</div>
+</div>
+</html>
--- siya/delete.php
+++ siya/delete.php
@@ -13,10 +13,10 @@ $conn = new mysqli($host,$user,$pass,$db);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 // sql to delete a record
-$sql = "DELETE FROM users WHERE user_id='" . $_POST["users"];
+$sql = "DELETE FROM users WHERE user_id='" . $_POST["users"]."'";
 
 if ($conn->query($sql) === TRUE) {
     echo "Record deleted successfully";
