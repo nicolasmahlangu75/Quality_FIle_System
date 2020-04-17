abd7b876c56f87b065031182d46f167fe9b3c458
Siya Dlamini <ssineliso@gmail.com>
2020-04-17 23:08:50 +0200

    dd

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -4,46 +4,52 @@
     <title>Admin Dashboard</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
     <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="css/animate.css">
-
+    
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/magnific-popup.css">
+
     <link rel="stylesheet" href="css/aos.css">
+
     <link rel="stylesheet" href="css/ionicons.min.css">
+
     <link rel="stylesheet" href="css/bootstrap-datepicker.css">
     <link rel="stylesheet" href="css/jquery.timepicker.css">
 
+    
     <link rel="stylesheet" href="css/flaticon.css">
     <link rel="stylesheet" href="css/icomoon.css">
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-
+    
 	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
 	      <a class="navbar-brand" href="index.html">Admin Dashboard</a>
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
+
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item"><a href="./index.php" class="nav-link">Home</a></li>
-	          <li class="nav-item"><a href="./about.php" class="nav-link">About Us</a></li>
-	          <li class="nav-item active"><a href="viewL.php" class="nav-link">View Lectures</a></li>
-	          <li class="nav-item"><a href="./search_delete.php" class="nav-link">Delete Lectures</li>
-	          <!-- <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li> -->
+	          <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="about.php" class="nav-link">About Us</a></li>
+	          <li class="nav-item active"><a href="viewLecture.php" class="nav-link">View Lectures</a></li>
+	          <li class="nav-item"><a href="delete.php" class="nav-link">Delete Lectures</li>
+	          <li class="nav-item"><a href="blog.html" class="nav-link">Log Out</a></li> 
 	           <!-- <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>-->
 	        </ul>
 	      </div>
 	    </div>
 	  </nav>
     <!-- END nav -->
-
+    
     <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.12">
       <div class="overlay"></div>
       <div class="container">
@@ -54,19 +60,23 @@
         </div>
       </div>
     </section>
+
           </div>
         </div>
         <div class="row">
           <div class="col-md-12 text-center">
-  Copyright ICEP&copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+  Copyright ICEP&copy;<script>document.write(new Date().getFullYear());</script> All rights reserved 
           </div>
         </div>
       </div>
     </footer>
-
+    
+  
 
   <!-- loader -->
- <!--<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>-->
+ <!--<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>--> 
+
+
   <script src="js/jquery.min.js"></script>
   <script src="js/jquery-migrate-3.0.1.min.js"></script>
   <script src="js/popper.min.js"></script>
